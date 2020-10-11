b6921aa63340c0565224cb4279548f23e02648b2
Jamir Hossain <46633915+jamircse@users.noreply.github.com>
2020-10-12 03:44:39 +0600

    Update 11.1 Dictionary Example.py

--- Python-3/11.1 Dictionary Example.py
+++ Python-3/11.1 Dictionary Example.py
@@ -1,11 +1,12 @@
  
 data1={1:'rahim',2:'karim'};
-print(data1);
+print(data1); # Print Dictionary
+
 for i in data1:
     print(data1[i]);
 
-print(data1.keys());
-print(data1.values());
-print(data1.items());
+print(data1.keys()); # print dictionary keys= 1 2
+print(data1.values()); # print dictionary values 
+print(data1.items()); 
 #print(data1.has_key(1));
 print(data1.get(2));
